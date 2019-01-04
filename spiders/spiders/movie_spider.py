b1bd0a54afea53c1fd7bf44ee4bf8949b2730465
Cho-Ching Lu <chochinlu@gmail.com>
2019-01-04 22:08:33 +0800

    movie comments spider

---
+++ spiders/spiders/movie_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class MovieSpider(scrapy.Spider):
+  name='movie'
+
+  header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+  }
+  start_urls = [
+    'https://movie.douban.com/subject/3878007/comments?status=P'
+  ]
+
+  def start_requests(self):
+       for url in self.start_urls:
+           yield scrapy.Request(url=url, callback=self.parse, headers=self.header)
+
+  def parse(self, response):
+    result = response.css('div.comment-item')
+    for r in result:
+      yield {
+        'author': r.css('a::text').extract()[3],
+        'watched': r.css('span.comment-info span::text').extract()[0],
+        'rating': r.css('span.comment-info span:nth-child(3)::attr(title)').extract()[0],
+        'commentTime': r.css('span.comment-time::text').extract()[0].replace(' ', '').replace('\n',''),
+        'comment': r.css('span.short::text').extract()[0].replace('\n', '')
+      }
+  
