eb0a5a68a3a2bc0c2aeca323dc9541d7e92994d4
Cho-Ching Lu <chochinlu@gmail.com>
2019-01-15 10:56:24 +0800

    use custom_settings.

--- spiders/spiders/movie_spider.py
+++ spiders/spiders/movie_spider.py
@@ -2,14 +2,16 @@ import scrapy
 class MovieSpider(scrapy.Spider):
   name='movie'
 
-  header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+  custom_settings = {
+    'DEFAULT_REQUEST_HEADERS': {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    }
   }
 
   def start_requests(self):
     url = 'https://movie.douban.com/subject/3878007/comments?status=P'
-    yield scrapy.Request(url=url, callback=self.parse, headers=self.header)
+    yield scrapy.Request(url=url, callback=self.parse)
 
   def parse(self, response):
     result = response.css('div.comment-item')
@@ -23,6 +25,6 @@ class MovieSpider(scrapy.Spider):
       }
 
     for a in response.css('div#paginator a.next'):
-      yield response.follow(a, callback=self.parse, headers=self.header)
+      yield response.follow(a, callback=self.parse)
     
   
