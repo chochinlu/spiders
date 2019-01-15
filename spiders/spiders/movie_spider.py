2f99ca8a278cf6e348f5c6a73116eb67a95e44a6
Cho-Ching Lu <chochinlu@gmail.com>
2019-01-15 10:22:39 +0800

    tune

--- spiders/spiders/movie_spider.py
+++ spiders/spiders/movie_spider.py
@@ -16,10 +16,10 @@ class MovieSpider(scrapy.Spider):
     for r in result:
       yield {
         'author': r.css('a::text').extract()[3],
-        'watched': r.css('span.comment-info span::text').extract()[0],
-        'rating': r.css('span.comment-info span:nth-child(3)::attr(title)').extract()[0],
-        'commentTime': r.css('span.comment-time::text').extract()[0].replace(' ', '').replace('\n',''),
-        'comment': r.css('span.short::text').extract()[0].replace('\n', '') 
+        'watched': r.css('span.comment-info span::text').extract_first(),
+        'rating': r.css('span.comment-info span.rating::attr(title)').extract_first(),
+        'commentTime': r.css('span.comment-time::text').extract_first().replace(' ', '').replace('\n',''),
+        'comment': r.css('span.short::text').extract_first().replace('\n', '') 
       }
 
     for a in response.css('div#paginator a.next'):
