4769d3b1226eea285af8d040ee8f6f0df99fc61d
Cho-Ching Lu <chochinlu@gmail.com>
2019-01-16 11:28:11 +0800

    Add common custom settings.

---
+++ spiders/spiders/custom_settings.py
@@ -0,0 +1,8 @@
+douban_settings = {
+    'DEFAULT_REQUEST_HEADERS': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    },
+    'ROBOTSTXT_OBEY': False,
+    'FEED_EXPORT_ENCODING': 'utf-8'
+}
